0f5197799b94e3a56c80f63a8220f073071dc621
꧁༒☬Ðєαтн☬༒꧂ <88191864+BadBlackBrine@users.noreply.github.com>
2022-10-19 16:18:52 +0530

    Add files via upload

---
+++ code100.py
@@ -0,0 +1,23 @@
+
+
+person = {
+    "name" : "Ishan",
+    "mobile" : 9090909090,
+    "email" : "demo@x.com"
+}
+
+print(type(person))
+
+
+print(person["name"])
+
+# -
+class ClassName:
+    def __init__(self , a):
+        # statements
+        print("hello")
+        self.a = a
+        print(self.a)
+
+jName = ClassName(100)                  
+
